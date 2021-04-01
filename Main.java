f640706477cb9c252d85c4bf9653c5d3832c7c4b
Zenzile <81443067+209045256@users.noreply.github.com>
2021-04-01 02:26:38 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.jddtest;
+
+/**
+ *
+ * @author 209045256
+ */
+public class Main {
+      public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+}
